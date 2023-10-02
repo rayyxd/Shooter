98552f5a1b055faa190b3322a2d9241ca5c29bff
rayyxd <sybbka.of@mail.ru>
2023-10-02 11:36:11 +0600

    fix commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 class Main{
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         Equipment ak47 = new AK47();
-        Equipment deagle = new Deagle();
-        Equipment defkits = new DefusingKits();
         Equipment glock = new Glock();
 
         ak47.performAttack1();
